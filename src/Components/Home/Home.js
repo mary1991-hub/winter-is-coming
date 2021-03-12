c0ac6ab45c1bb3ed53ba78ccf83f9a97e0be276d
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-03-11 19:15:06 -0800

    setup background image and header on home page

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+    return (
+        <main className="Hero">
+            <h1 className="Hero__Header">winter is coming</h1>
+            
+        </main>
+    )
+}
+
+export default Home;
