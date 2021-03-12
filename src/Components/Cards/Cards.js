9ad0fe614d7012556dffb1d0ba1aadcba54a9ad7
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-03-12 10:35:27 -0800

    working on cards section

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -8,9 +8,17 @@ function Cards(props) {
         <section className="Cards">
           {props.characters.map(character => {
               return (
-                  <ul key={character.id}>
-                      <li className="text">{character.firstName}</li>
+                  <div key={character.id}>
+                  <ul className="Cards__Front--Container">
+                      <li className="Cards__List" className="text">{character.image}</li>
                   </ul>
+                  <ul className="Cards__Back--Container">
+                      <li className="Cards__Family">{character.family}</li>
+                      <li className="Cards__FullName">{character.fullName}</li>
+                      <li className="Cards__Title">{character.title}</li>
+                      <li>What is the actors real name?  Make your guess!</li>
+                  </ul>
+                  </div>
               )
           })}
             
