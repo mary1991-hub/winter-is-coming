f3ec4213098614de8c16af9ea89df1b7b206dd89
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-03-11 20:50:56 -0800

    got main page up trying to map data with componentdidupdate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import Home from './Components/Home/Home';
+import Cards from './Components/Cards/Cards';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+// const GOT__URL = 'https://thronesapi.com/api/v2/Characters';
+
+
+const API__KEY = '?api_key=<9455f564-3386-4190-beee-47776f219b2a>'
+const API__URL = 'https://project-2-api.herokuapp.com'
+
+class App extends React.Component {
+
+  state = {
+    characters: []
+  }
+
+  componentDidMount() {
+    axios.get(`${API__URL}/videos${API__KEY}`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        characters: res.data
+      })
+    }
+    )} 
+
+  // componentDidUpdate(prevProps) {
+  //   if(prevProps !== this.state.characters.id)
+  //   axios.get(`${API__URL}/videos${API__KEY}`)
+  //   .then(res => {
+  //     console.log(res.data)
+  //     this.setState({
+  //       characters: res.data
+  //     })
+  //   }
+  //   )} 
+  
+
+  render() {
+    console.log('Characters: ', this.state.charcters)
   return (
     <div className="App">
       <Home />
+      <Cards characters={this.state.characters}/>
     </div>
   );
+}
 }
 
 export default App;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Home.scss';
 
-function Home() {
+class Home extends React.Component {
+
+
+   
+
+    render(props) {
     return (
         <main className="Hero">
             <h1 className="Hero__Header">winter is coming</h1>
-            
         </main>
     )
+}
 }
 
 export default Home;
